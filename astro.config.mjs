94b8902b46af3f5eff1eec245c154fd79b23735e
PaulSenon <paul.senon@gmail.com>
2024-12-13 16:12:56 +0900

    add cache only for static routes

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,7 +14,6 @@ export default defineConfig({
 	site: NETLIFY_DEPLOY_URL || 'https://localhost:4321',
 	output: 'static',
 	adapter: netlify({
-		cacheOnDemandPages: true,
 		// edgeMiddleware: true,
 	}),
 	integrations: [
