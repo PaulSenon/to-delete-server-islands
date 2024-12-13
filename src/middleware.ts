6a73dfd9357d2743d12039d6d9bf12b52281c9bf
PaulSenon <paul.senon@gmail.com>
2024-12-13 17:48:23 +0900

    log instead

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,23 +1,22 @@
 import { defineMiddleware } from "astro/middleware";
 
 export const onRequest = defineMiddleware(async ({ locals, request }, next) => {
-  // Generate a random value
-  const randomValue = Math.random().toString(36).substring(2, 15);
-  
-  // Add the random value to locals so it's accessible in components
-  locals.requestId = Math.random()*1000;
-  //@ts-ignore
-  locals.netlify.context.toto = Math.random()*1000;
-  
-  // Get the response from the next handler
-  const response = await next();
-  
-  
-  // Clone the response to make it mutable
-  const newResponse = new Response(response.body, response);
-  
-  // Add our custom header
-  newResponse.headers.set('X-Random-Value', randomValue);
-  
-  return newResponse;
+    console.log(locals);
+    // Generate a random value
+    const randomValue = Math.random().toString(36).substring(2, 15);
+    
+    // Add the random value to locals so it's accessible in components
+    locals.requestId = Math.random()*1000;
+    
+    // Get the response from the next handler
+    const response = await next();
+    
+    
+    // Clone the response to make it mutable
+    const newResponse = new Response(response.body, response);
+    
+    // Add our custom header
+    newResponse.headers.set('X-Random-Value', randomValue);
+    
+    return newResponse;
 }) 
