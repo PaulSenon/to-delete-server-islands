859c2e2485fb66bd272688afc1b53325e648aa16
PaulSenon <paul.senon@gmail.com>
2024-12-13 17:02:36 +0900

    make custom middleware useless

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,20 +1,21 @@
 import { defineMiddleware } from "astro/middleware";
 
 export const onRequest = defineMiddleware(async ({ locals, request }, next) => {
-  // Generate a random value
-  const randomValue = Math.random().toString(36).substring(2, 15);
+//   // Generate a random value
+//   const randomValue = Math.random().toString(36).substring(2, 15);
   
-  // Add the random value to locals so it's accessible in components
-  locals.requestId = Math.random()*1000;
+//   // Add the random value to locals so it's accessible in components
+//   locals.requestId = Math.random()*1000;
   
   // Get the response from the next handler
-  const response = await next();
+    const response = await next();
+    return response;
   
   // Clone the response to make it mutable
-  const newResponse = new Response(response.body, response);
+//   const newResponse = new Response(response.body, response);
   
-  // Add our custom header
-  newResponse.headers.set('X-Random-Value', randomValue);
+//   // Add our custom header
+//   newResponse.headers.set('X-Random-Value', randomValue);
   
-  return newResponse;
+//   return newResponse;
 }) 
