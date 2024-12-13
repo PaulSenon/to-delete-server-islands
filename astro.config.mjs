52856983f447e1a7051bb66cd3ed46491e4892bd
PaulSenon <paul.senon@gmail.com>
2024-12-13 16:26:30 +0900

    enable edge middleware

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,7 +14,7 @@ export default defineConfig({
 	site: NETLIFY_DEPLOY_URL || 'https://localhost:4321',
 	output: 'static',
 	adapter: netlify({
-		// edgeMiddleware: true,
+		edgeMiddleware: true,
 	}),
 	integrations: [
 		react(),
